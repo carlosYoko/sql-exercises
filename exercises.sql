cc37c2b1283838f0d17f5e5b47e3fb0e320f0faa
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-08-11 15:17:41 +0200

    exercices page 24

--- exercises.sql
+++ exercises.sql
@@ -6,3 +6,13 @@ select NOMBRE, APELLIDOS, F_NACIMIENTO from empleados e where salario > 1350;
 select NOMBRE, APELLIDOS, F_NACIMIENTO from empleados e where salario >= 1350 and salario <= 1450;
 -- equivalente con operador between
 select NOMBRE, APELLIDOS, F_NACIMIENTO from empleados e where salario between 1350 and 1450;
+
+-- e34
+select nombre, apellidos from empleados e  where salario < 1350;
+
+select nombre from personas p  where rubia = 'S' and alta = 's';
+
+select * from empleados e where ID_EMPLEADO = 1 or ID_EMPLEADO  = 2 or ID_EMPLEADO = 4;
+-- equivalente con el operador IN
+select * from empleados e where ID_EMPLEADO in (1,2,4);
+
