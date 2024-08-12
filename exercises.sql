e9c11f58deb2810d5608bfc8790986f93911bd92
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-08-12 20:18:26 +0200

    refactor

--- exercises.sql
+++ exercises.sql
@@ -1,3 +1,7 @@
+-- Primera parte --
+-- Capitulo 2 --
+-- Consultas SQL --
+
 select NOMBRE, APELLIDOS from empleados e where salario > 1350; 
 
 -- e29
@@ -6,6 +10,9 @@ select NOMBRE, APELLIDOS, F_NACIMIENTO from empleados e where salario > 1350;
 select NOMBRE, APELLIDOS, F_NACIMIENTO from empleados e where salario >= 1350 and salario <= 1450;
 -- equivalente con operador between
 select NOMBRE, APELLIDOS, F_NACIMIENTO from empleados e where salario between 1350 and 1450;
+
+-- Capitulo 3 --
+-- Consultas SQL II --
 
 -- e34
 select nombre, apellidos from empleados e  where salario < 1350;
@@ -16,6 +23,9 @@ select * from empleados e where ID_EMPLEADO = 1 or ID_EMPLEADO  = 2 or ID_EMPLEA
 -- equivalente con el operador IN
 select * from empleados e where ID_EMPLEADO in (1,2,4);
 
+-- Capitulo 5 --
+-- Operadores --
+
 -- e1.57
 select * from personas p where ALTA = 'S' or (RUBIA = 'S' and GAFAS = 'S');
 
@@ -25,25 +35,30 @@ select * from empleados e where SEXO = 'M' and SALARIO > 1300;
 -- e.3.58
 select  * from personas p where GAFAS = 'S' and RUBIA = 'N' and ALTA = 'N'
 
+-- Capitulo 6 --
+-- Funciones de agregado--
+
 select sum(salario) from empleados;
 
 select count(*) from empleados e;
 
--- Funciones de agregado
 select sum(salario) / count(*) as SALARIO_MEDIO from empleados e;
 -- equivalente para calcular el svg
 select avg(salario) as SALARIO_MEDIO from empleados e;
 
 select max(salario) from empleados e;
 
--- e1.63
+-- e1.68
 select avg(salario) as SALARIO_MEDIO_HOMBRES from empleados e where SEXO = 'H';
 
--- e2.63
+-- e2.68
 select max(salario) as SALARIO_MAXIMO, min(salario ) as SALARIO_MINIMO from empleados e;
 
--- e3.63
+-- e3.68
 select sum(salario) as TOTAL_A_PAGAR from empleados e where SEXO = 'M'; 
+
+-- Capitulo 7 --
+-- Agrupacion de datos--
 
 select sexo, count(*)as TOTAL_EMPLEADOS from empleados e
 group by sexo;
@@ -67,6 +82,14 @@ from mascotas m
 where ESTADO = 'A'
 group by UBICACION, especie;
 
+-- e4.80
+select distinct ubicacion, ESPECIE 
+from mascotas m 
+where estado = 'A'
+
+-- Capitulo 8 --
+-- Filtrar calculos de totalizacion --
+
 select Ubicacion, count(*) as total
 from mascotas 
 where estado = 'A' 
@@ -80,6 +103,25 @@ where ESTADO = 'A'
 group by UBICACION
 having Ejemplares between 2 and 3;
 
+-- Capitulo 9 --
+-- Ordenacion del resultado --
+
+-- e1.92
+select *
+from personas p
+order by p.RUBIA desc, p.ALTA desc, p.GAFAS;
+
+-- e2.92
+select UBICACION, count(*) as TOTAL_EJEMPLARES 
+from mascotas m
+where ESTADO = 'A'
+group  by UBICACION 
+having TOTAL_EJEMPLARES > 1
+order by TOTAL_EJEMPLARES DESC, UBICACION 
+
+-- Capitulo 10 --
+-- Operador Like y el valor Null --
+
 -- e1.101
 select * from empleados e
 where APELLIDOS like '%calvo%';
@@ -87,6 +129,10 @@ where APELLIDOS like '%calvo%';
 -- e2.101
 select * from vehiculos v
 where ULTI_ITV is null and PROX_ITV between '20110101' and '20111231'
+
+-- Segunda Parte --
+-- Capitulo 12 --
+-- Producto cartesiano --
 
 select camisa, pantalon, c.PESO_GR + p.PESO_GR as peso
 from camisas c, pantalones p 
@@ -97,6 +143,9 @@ from camisas c , pantalones p , calzados c2
 
 -- e1.120
 select p.PANTALON, c.CALZADO from pantalones p , calzados c 
+
+-- Capitulo 13 --
+-- Abstraccion de tabla --
 
 select c.ID_CAMISA , c.CAMISA , c.PESO_GR as Peso_Camisa,
 		p.ID_PANTALON , p.PANTALON , p.PESO_GR as Peso_Pantalon
@@ -115,10 +164,12 @@ from camisas c , pantalones p
 group by c.CAMISA
 having peso_medio > 850
 
+-- e3.130
 select c.CAMISA , p.PANTALON 
 from camisas c , pantalones p
 where  c.ID_CAMISA  != p.ID_PANTALON
 
+-- e4.130
 select concat('Camisa ', c.CAMISA) as prenda
 from camisas c 
 union all
@@ -127,6 +178,9 @@ from  pantalones p
 union all
 select concat (c2.calzado)
 from calzados c2 
+
+-- Capitulo 14 --
+-- Relacions, claves primarias y foraneas --
 
 -- e1.144
 select C.TITULO 
@@ -161,6 +215,9 @@ where  ac.ID_CURSO = c.ID_CURSO
 group by C.TITULO
 having TOTAL_ALUMNOS = 2;
 
+-- Capitulo 15 --
+-- Reunion interna y externa --
+
 select c.TITULO as CURSO, count(ac.ID_ALUMNO) as ALUMNOS, COUNT(C.TITULO) as REGISTROS
 from alumnos_cursos ac right join cursos c
 on ac.ID_CURSO = c.ID_CURSO 
@@ -187,5 +244,4 @@ group by c.TITULO
 select c.TITULO, a.NOMBRE , a.APELLIDOS 
 from cursos c join alumnos_cursos ac on c.ID_CURSO = ac.ID_CURSO join alumnos a on a.ID_ALUMNO = ac.ID_ALUMNO 
 group by c.TITULO , a.NOMBRE , a.APELLIDOS 
-order by c.TITULO 
-
+order by c.TITULO
